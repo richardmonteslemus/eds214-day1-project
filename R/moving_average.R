ec87db40fa34d5250eba772e328d7fd586ed94f2
richardmonteslemus <richard.montes.lemus@gmail.com>
2025-08-28 11:18:06 -0700

    quarto analysis

---
+++ R/moving_average.R
@@ -0,0 +1,11 @@
+moving_average <- function(focal_date, dates, conc, win_size_wks) {
+  # Which dates are in the window?
+  is_in_window <- (dates > focal_date - (win_size_wks/2) * 7) & 
+    (dates < focal_date + (win_size_wks / 2) * 7)
+  # Find the associated concentrations
+  window_conc <- conc[is_in_window]
+  # Calculate the mean 
+  result <- mean(window_conc, na.rm = TRUE)
+  
+  return(result)
+}
